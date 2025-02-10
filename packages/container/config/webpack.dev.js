11c1554c449919505b4e91a12cc8ff256fc66aa8
Alexander Mahterian <unit43.dev@gmail.com>
2025-02-10 18:24:10 -0500

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,34 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+// common config
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            // if you want to be specific, use shared: ['react', 'etc]
+            shared: packageJson.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
+
+
